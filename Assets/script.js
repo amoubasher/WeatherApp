458fcd65ae668449fc934247beb8f9a0e877a594
Amr Moubasher <amoubasher@gmail.com>
2022-11-07 16:51:45 -0800

    did some JS ran into errors

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,27 @@
 // Server-Side APIs Day 3 01:25:46
 // Server-Side APIs Day 3 01:43:24
+// Server-Side APIs Day 3 02:00:00
 
-var api_key = '4253ae682bded8fe54667e18d996e279';
+var api_key = 'eb7f528cc275c1424ff1ab19a726e7c7';
 function getGeoLocation (query, limit = 5) {
-    return fetch('http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${5}&appid=${api_key}')
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${5}&appid=${api_key}`)
 }
 
 
 function getCurrentWeather({arguments}){
-    return fetch ('')
+    // https://api.openweathermap.org/data/3.0
+    return fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid={API key}`)
     // ${arguments.lan} & ${arguments.lon} & ${imperial} & ${api_key}
 }
 
 
 getGeoLocation('Anaheim')
-.then(response => response.json())
+.then(function(response){
+    return response.json()
+})
 .then(data => {
-    var {lat, lon} = data[0];
-    getCurrentWeather({lat: data[0].lat, lon: data[0].lon})
+    console.log(data)
+    getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
     .then(weatherResponse => weatherResponse.json())
     .then(weatherData => {
         document.body.textContent = JSON.stringify(weatherData, null, 2)
