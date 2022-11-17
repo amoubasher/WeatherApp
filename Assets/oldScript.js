ea66eb5664379012e91c84458558a898df798045
amoubasher <amoubasher@gmail.com>
2022-11-17 01:42:11 -0800

    Added extra logic to JS

---
+++ Assets/oldScript.js
@@ -0,0 +1,64 @@
+// Server-Side APIs Day 3 01:25:46
+// Server-Side APIs Day 3 01:43:24
+// Server-Side APIs Day 3 02:00:00
+// Server-Side APIs Day 3 02:13:15
+
+var input = document.querySelector('#input')
+
+input.addEventListener('keyup', function(event){
+    if (event.key === 'Enter'){
+        createWeatherDisplay(event.target.value)
+    }
+})
+
+// var button = document.querySelector('#button')
+// button.addEventListener('click', function(event){
+//     if (event.click === 'click'){
+//         createWeatherDisplay(event.target.value)
+//     }
+// })
+
+var api_key = 'eb7f528cc275c1424ff1ab19a726e7c7';
+function getGeoLocation (query, limit = 5) {
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${5}&appid=${api_key}`)
+}
+
+
+function getCurrentWeather(arguments){
+    // https://api.openweathermap.org/data/3.0
+    return fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid=${api_key}`)
+    // ${arguments.lan} & ${arguments.lon} & ${imperial} & ${api_key}
+}
+
+function createWeatherDisplay(location){
+    return getGeoLocation(location)
+    .then(function(response){
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        if (data.length === 0){
+            var errorEl = document.createElement('p')
+            errorEl.textContent = `We could not find ${location}`
+            document.body.appendChild(errorEl)
+        } else {
+            getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
+            .then(weatherResponse => weatherResponse.json())
+            .then(weatherData => {
+                var weatherPicture = document.createElement('img')
+                weatherPicture.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+                var currentWeatherStatement = document.createElement('p')
+                currentWeatherStatement.textContent = `${weatherData.weather[0].main}: it is currently ${weatherData.weather[0].description}`
+                document.body.appendChild(weatherPicture)
+                document.body.appendChild(currentWeatherStatement)
+                // document.body.textContent = JSON.stringify(weatherData, null, 2)
+            })
+            .catch(error => {
+                document.body.textContent = error.message
+            })   
+        }
+    })
+    .catch(error => {
+        document.body.textContent = error.message
+    })
+}
--- Assets/script.js
+++ Assets/script.js
@@ -4,32 +4,36 @@
 
 var api_key = 'eb7f528cc275c1424ff1ab19a726e7c7';
 function getGeoLocation (query, limit = 5) {
-    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${5}&appid=${api_key}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${5}&appid=${api_key}`).then(function(response){
+        return response.json()
+    })
+    .then(data => {
+        console.log(data[0].lat)
+        getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
+        .then(weatherResponse => weatherResponse.json())
+        .then(weatherData => {
+            //now we can consume data
+            displayWeatherData(weatherData)
+        })
+        .catch(error => {
+            console.log("error", error)
+            document.body.textContent = error.message
+        })
+    })
+}
+
+function displayWeatherData(data){
+    //do stuff with data here, DOM manipulation logic
+    document.body.textContent = JSON.stringify(data, null, 2);
 }
 
 
-function getCurrentWeather({arguments}){
+function getCurrentWeather({lat,lon}){
     // https://api.openweathermap.org/data/3.0
-    return fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid={API key}`)
+    return fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${'imperial'}&appid=${api_key}`)
     // ${arguments.lan} & ${arguments.lon} & ${imperial} & ${api_key}
 }
 
+getGeoLocation('Irvine');
 
-getGeoLocation('Anaheim')
-.then(function(response){
-    return response.json()
-})
-.then(data => {
-    console.log(data)
-    getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
-    .then(weatherResponse => weatherResponse.json())
-    .then(weatherData => {
-        document.body.textContent = JSON.stringify(weatherData, null, 2)
-    })
-    .catch(error => {
-        document.body.textContent = error.message
-    })
-})
-.catch(error => {
-    document.body.textContent = error.message
-})
+
